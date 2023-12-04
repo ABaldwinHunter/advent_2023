47ea73672c8f69c328ffac948e0691638b2e920b
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2023-12-04 00:37:44 -0500

    Recursion baby

--- 04/solution.rb
+++ 04/solution.rb
@@ -4,7 +4,7 @@
 file = 'input.txt'
 cards = File.read(file).split("\n")
 
-def points(card)
+def get_hits(card)
   contents = card.split(":").last
 
   winning, mine = contents.split("|")
@@ -19,6 +19,12 @@ def points(card)
     hits += 1 if my_numbers.include?(number)
   end
 
+  hits
+end
+
+def points(card)
+  hits = get_hits(card)
+
   if hits == 0
     0
   elsif hits == 1
@@ -28,5 +34,65 @@ def points(card)
   end
 end
 
+# part 1
+#
 puts cards.sum { |card| points(card) }
+
+# part 2
+#
+# winning scratch cards
+#
+
+# there are 201 cards
+
+# card_counts = {}
+
+# cards.each.with_index do |card, i|
+#   number = i + 1
+
+#   card_counts[number] = 1 # for the first
+
+#   hits = get_hits(card)
+
+#   number..(number + hits).each do |card_number|
+#     current = card_counts[card_number]
+
+#     if current.nil? || current == 1
+#       card_counts[card_number] += 1
+#     end
+#   end
+# end
+
+hits_matrix = {}
+
+cards.each.with_index do |card, i|
+  hits_matrix[i] = get_hits(card)
+end
+
+MATRIX = hits_matrix
+
+def gained_cards(card_number)
+  if card_number > 200
+    0
+  else
+    if MATRIX[card_number] == 0
+      0
+    else
+      hits = MATRIX[card_number]
+
+      next_card = card_number + 1
+      last = card_number + hits
+
+      hits + (next_card..(last)).sum { |card| gained_cards(card) }
+    end
+  end
+end
+
+total = 201 # to start
+
+MATRIX.keys.each.with_index do |card_number, i|
+  total += gained_cards(card_number)
+end
+
+puts total
 
