25f4d557f748a6f018978ad2895fcc4fe92c77ae
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2023-12-10 23:55:26 -0500

    Add comment and fix bug

--- 03/solution.rb
+++ 03/solution.rb
@@ -1,5 +1,5 @@
-# file = File.read("input.txt")
-file = File.read("sample.txt")
+file = File.read("input.txt")
+# file = File.read("sample.txt")
 
 rows = file.split("\n") # [ "fsdkflksdjf", "zdfsfsdf"]
 grid = rows.map { |row| row.split("") }
@@ -11,18 +11,18 @@ grid = rows.map { |row| row.split("") }
 row_coord = 0
 col_coord = 0
 
-max_row = rows.length - 1
-max_col = rows.first.length - 1
+MAX_ROW = rows.length - 1
+MAX_COL = rows.first.length - 1
 
 numbers = [] # [number, start_cooords, end_coords]
 
-while row_coord <= max_row do
+while row_coord <= MAX_ROW do
 
   curr_number_start = nil
   curr_number_end = nil
   curr_number = ''
 
-  while col_coord <= max_col do
+  while col_coord <= MAX_COL do
     curr_square = grid[row_coord][col_coord]
 
     if curr_square.match(/\d/)
@@ -32,7 +32,7 @@ while row_coord <= max_row do
 
       curr_number += curr_square
 
-      if (col_coord == max_col)
+      if (col_coord == MAX_COL)
         curr_number_end = [row_coord, col_coord]
       end
     else
@@ -57,8 +57,67 @@ while row_coord <= max_row do
   row_coord += 1
 end
 
-pp numbers
+# pp numbers
+#
 
+# then, iterate through the numbers and select any with symbols adjacent
+#
 
+def get_adjacent_squares(number_with_data)
+  number = number_with_data.first
+  start_coords = number_with_data[1] # row, col
+  end_coords = number_with_data[2]
 
+  row = start_coords[0]
+  cols = [start_coords[1], end_coords[1]]
 
+  positions_to_check = []
+
+  # one to the left
+  positions_to_check << [row, (start_coords[1] - 1)] unless start_coords[1] == 0
+  # one to the right
+  positions_to_check << [row, (end_coords[1] + 1)] unless end_coords[1] == MAX_COL
+
+  cols = Range.new(start_coords[1], end_coords[1]).to_a
+
+  # above
+  above = cols.map { |col| [(row - 1), col] }
+  positions_to_check += above if row != 0
+  # below
+  below = cols.map { |col| [(row + 1), col] }
+  positions_to_check += below if row != MAX_ROW
+
+  # diagonal - 4
+
+  diags = [
+    [(row - 1), start_coords[1] - 1],
+    [(row - 1), end_coords[1] + 1],
+    [(row + 1), start_coords[1] - 1],
+    [(row + 1), end_coords[1] + 1],
+  ]
+
+  cleaned_diags = diags.reject { |pair| pair.first < 0 || pair.first > MAX_ROW || pair.last < 0 || pair.last > MAX_COL }
+
+  positions_to_check += cleaned_diags
+end
+
+sum = 0
+
+numbers.each do |number_with_data|
+  adjacent_coords = get_adjacent_squares(number_with_data)
+
+  hit = adjacent_coords.detect do |coords|
+
+    square = grid[coords.first][coords.last]
+
+    square != '.' && !['123456789'].include?(square) # then special char
+  end
+
+  sum += number_with_data.first if hit
+end
+
+puts sum
+
+# first attempt 527252 - too high (meanwhile the sample is correct!)
+#
+# second attempt 525181 - correct! (problem was no filtering out diags with the reject)
